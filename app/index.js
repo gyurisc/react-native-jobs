1ef5a52dfc5fc42fdafc43f2a0eeb5dcf522c045
gyurisc <gyurisc@gmail.com>
2023-09-14 06:25:19 +0200

    starting up expo

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Home Text - This is a React Native App Here</Text>
+        </View>
+    )
+
+}
+
+export default Home;
